2e1363e27e12ba59cc99085a60f3e56d95c58e95
MShandilya707 <146753545+MShandilya707@users.noreply.github.com>
2025-02-15 15:24:34 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This allows external access
+    port: process.env.PORT || 4173,  // Use Railway's assigned port
+  }
 })
